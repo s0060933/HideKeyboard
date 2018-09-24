d5bee9d5e55f4981c8e8c893d28dd1c5e1993b66
s0060933 <43101315+s0060933@users.noreply.github.com>
2018-09-24 18:01:11 -0500

    Add files via upload

---
+++ HideKeyboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HideKeyboard
+//
+//  Created by cis290 on 9/24/18.
+//  Copyright © 2018 Rock Valley College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
