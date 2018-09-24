9a8987abe07e67a835d9ee71e095e248edad79d9
s0060933 <43101315+s0060933@users.noreply.github.com>
2018-09-24 18:41:34 -0500

    Add files via upload

--- HideKeyboard/ViewController.swift
+++ HideKeyboard/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnBack: UIButton!
+    @IBOutlet weak var txtView: UITextView!
+    
+    @IBAction func btnBack(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
